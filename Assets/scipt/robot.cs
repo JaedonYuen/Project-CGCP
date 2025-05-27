6954463d6c3ab326df504232ff50af298185fc2e
JaedonYuen <2402335J@student.tp.edu.sg>
2025-05-27 17:50:58 +0800

    gundam

--- Assets/scipt/robot.cs
+++ Assets/scipt/robot.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using Unity.VisualScripting;
 
 
 
@@ -16,8 +17,8 @@ public class TransformValue
     public float rotationSpeed = 5f;
     public bool useSlerp = true;
 
-    public bool applyRotation = true;
-    public bool applyPosition = true;
+    public bool applyRotation = false;
+    public bool applyPosition = false;
 }
 [Serializable]
 public class Pose 
@@ -54,48 +55,6 @@ public class robot : MonoBehaviour
 
     private bool needToRest = false;
     private string animationAfterResting = "";
-    void Start()
-    {
-        //for some odd reason my code doesnt like forcibliy transforming the limbs back so a loophole i found is i make a default rest animation that diffrent limbs can fall back to
-        foreach (RobotAnimation robotAnimation in animations)
-        {
-            foreach (Pose p in robotAnimation.Poses)
-            {
-                foreach (TransformValue tv in p.TransformValues)
-                {
-                    tv.startPosition = tv.target.transform.localPosition;
-                    //add target to the list of targets in use if it is not already in the list
-                    if (!targetsInUse.Contains(tv.target))
-                    {
-                        targetsInUse.Add(tv.target);
-                    }
-                }
-            }
-        }
-        //we should initialize to prevent odd things at the start
-        Pose restingPose = new Pose();
-        restingPose.name = "rest";
-        foreach (GameObject target in targetsInUse)
-        {
-            TransformValue tv = new TransformValue();
-            tv.target = target;
-            tv.startPosition = target.transform.localPosition;
-            tv.rotation = Vector3.zero;
-            tv.position = target.transform.localPosition;
-            tv.applyRotation = true;
-            tv.applyPosition = true;
-            tv.rotationSpeed = 50f;
-            restingPose.TransformValues.Add(tv);
-        }
-        RobotAnimation restingAnimation = new RobotAnimation();
-        restingAnimation.name = "rest";
-        restingAnimation.loop = true;
-        restingAnimation.Poses.Add(restingPose);
-        animations.Add(restingAnimation);
-
-        
-    }
-
 
     //another odd thing about unity and math in general is that slerp, which is what i use to make smooth animations, isnt accurate to some degree, which makes my code a broken, but luckily i can just make these funtions to sort of give my code tolerance.
     bool ballParkRotation(Quaternion a, Quaternion b, float tolerance)
@@ -110,6 +69,131 @@ public class robot : MonoBehaviour
         return Vector3.Distance(a, b) < tolerance;
     }
 
+    void Awake()
+    {
+        // -- ANIMATONS -- //
+        // i felt like storing in the values in a table would make the code a bit more modular, allowing for anything to be animated.
+        // make some animations, we do this first so that we can see the used limbs when adding the rest animation
+        // fun fact, previously i would have used the unity table system but i felt that it wasnt very "code in the animations" so i just ported them here.
+        // walking
+        RobotAnimation walkingAnimation = new RobotAnimation(); // create main animation
+        walkingAnimation.name = "walk";
+        walkingAnimation.loop = true;
+
+        Pose walkPose1 = new Pose(); //pose 1, raise left leg, kick right leg back
+        walkPose1.name = "walkP1";
+
+        TransformValue walkP1T1 = new TransformValue(); //raise upper left leg up
+        walkP1T1.target = GameObject.Find("UpperLeftLeg");
+        walkP1T1.rotation = new Vector3(40, 0, 0);
+        walkP1T1.applyRotation = true;
+        walkPose1.TransformValues.Add(walkP1T1);
+
+        TransformValue walkP1T2 = new TransformValue(); //droop the lower left leg down
+        walkP1T2.target = GameObject.Find("LowerLeftLeg");
+        walkP1T2.rotation = new Vector3(-40, 0, 0);
+        walkP1T2.applyRotation = true;
+        walkPose1.TransformValues.Add(walkP1T2);
+
+        TransformValue walkP1T3 = new TransformValue(); //kick back the right leg
+        walkP1T3.target = GameObject.Find("UpperRightLeg");
+        walkP1T3.rotation = new Vector3(-40, 0, 0);
+        walkP1T3.applyRotation = true;
+        walkPose1.TransformValues.Add(walkP1T3);
+
+        TransformValue walkP1T4 = new TransformValue(); 
+        walkP1T4.target = GameObject.Find("LowerRightLeg");
+        walkP1T4.rotation = new Vector3(0, 0, 0);
+        walkP1T4.applyRotation = true;
+        walkPose1.TransformValues.Add(walkP1T4);
+
+        walkingAnimation.Poses.Add(walkPose1); //add the pose
+
+        Pose walkPose2 = new Pose(); //pose2, kick back left leg, raise right leg
+
+        TransformValue walkP2T1 = new TransformValue(); //raise upper right leg up
+        walkP2T1.target = GameObject.Find("UpperRightLeg");
+        walkP2T1.rotation = new Vector3(40, 0, 0);
+        walkP2T1.applyRotation = true;
+        walkPose2.TransformValues.Add(walkP2T1);
+
+        TransformValue walkP2T2 = new TransformValue(); //droop the lower right leg down
+        walkP2T2.target = GameObject.Find("LowerRightLeg");
+        walkP2T2.rotation = new Vector3(-40, 0, 0);
+        walkP2T2.applyRotation = true;
+        walkPose2.TransformValues.Add(walkP1T2);
+
+        TransformValue walkP2T3 = new TransformValue(); //kick back the left leg
+        walkP2T3.target = GameObject.Find("UpperLeftLeg");
+        walkP2T3.rotation = new Vector3(-40, 0, 0);
+        walkP2T3.applyRotation = true;
+        walkPose2.TransformValues.Add(walkP2T3);
+
+        TransformValue walkP2T4 = new TransformValue(); 
+        walkP2T4.target = GameObject.Find("LowerLeftLeg");
+        walkP2T4.rotation = new Vector3(0, 0, 0);
+        walkP2T4.applyRotation = true;
+        walkPose2.TransformValues.Add(walkP2T4);
+
+        walkingAnimation.Poses.Add(walkPose2); //add the pose
+
+        
+        animations.Add(walkingAnimation);
+    }
+
+    void Start()
+    {
+        
+        
+
+        //for some odd reason my code doesnt like forcibliy transforming the limbs back so a loophole i found is i make a default rest animation that diffrent limbs can fall back to
+        foreach (RobotAnimation robotAnimation in animations)
+        {
+            foreach (Pose p in robotAnimation.Poses)
+            {
+                foreach (TransformValue transform in p.TransformValues)
+                {
+                    transform.startPosition = transform.target.transform.localPosition;
+                    //add target to the list of targets in use if it is not already in the list
+                    if (!targetsInUse.Contains(transform.target))
+                    {
+                        targetsInUse.Add(transform.target);
+                    }
+                }
+            }
+        }
+        //we should initialize to prevent odd things at the start
+        Pose restingPose = new Pose();
+        restingPose.name = "rest";
+        foreach (GameObject target in targetsInUse)
+        {
+            TransformValue transform = new TransformValue();
+            transform.target = target;
+            transform.startPosition = target.transform.localPosition;
+            transform.rotation = Vector3.zero;
+            transform.position = target.transform.localPosition;
+            transform.applyRotation = true;
+            transform.applyPosition = true;
+            transform.rotationSpeed = 50f;
+            restingPose.TransformValues.Add(transform);
+        }
+        RobotAnimation restingAnimation = new RobotAnimation();
+        restingAnimation.name = "rest";
+        restingAnimation.loop = true;
+        restingAnimation.Poses.Add(restingPose);
+        animations.Add(restingAnimation);
+
+        
+
+
+
+
+
+        
+    }
+
+
+    
     // Update is called once per frame
     void Update()
     {
